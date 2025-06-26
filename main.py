f9e1cf27ba47b4e0050df78b7e7edfe06540e716
Sahil Shaik <sahilshaaik@gmail.com>
2025-06-26 19:33:29 +1000

    Initial

--- main.py
+++ main.py
@@ -1,22 +1,63 @@
-
 import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 
-##### TODO #########################################
-### IMPLEMENT 'getMyPosition' FUNCTION #############
-### TO RUN, RUN 'eval.py' ##########################
+trained_model = None
+last_training_day = -1
 
-nInst = 50
-currentPos = np.zeros(nInst)
+def get_features(prices):
+    nInst, nDays = prices.shape
+    df = pd.DataFrame(prices.T)
+    momentum_5 = (prices[:, 5:] - prices[:, :-5]) / prices[:, :-5]
+    momentum_10 = (prices[:, 10:] - prices[:, :-10]) / prices[:, :-10]
+    momentum_20 = (prices[:, 20:] - prices[:, :-20]) / prices[:, :-20]
+    ma_5 = df.rolling(5).mean().values.T
+    ma_20 = df.rolling(20).mean().values.T
+    std_20 = df.rolling(20).std().values.T
+    ma_diff = ma_5 - ma_20
+    z_score = (prices - ma_20) / (std_20 + 1e-6)
+    std_ratio = std_20 / (ma_20 + 1e-6)
+    min_len = nDays - 20
+    features = np.stack([
+        momentum_5[:, -min_len:],
+        momentum_10[:, -min_len:],
+        momentum_20[:, -min_len:],
+        z_score[:, -min_len:],
+        ma_diff[:, -min_len:],
+        std_ratio[:, -min_len:]
+    ], axis=-1)
+    return features
 
+def get_labels(prices):
+    next_day_returns = (prices[:, 21:] - prices[:, 20:-1]) / (prices[:, 20:-1] + 1e-6)
+    return (next_day_returns > 0).astype(int)
 
-def getMyPosition(prcSoFar):
-    global currentPos
-    (nins, nt) = prcSoFar.shape
-    if (nt < 2):
-        return np.zeros(nins)
-    lastRet = np.log(prcSoFar[:, -1] / prcSoFar[:, -2])
-    lNorm = np.sqrt(lastRet.dot(lastRet))
-    lastRet /= lNorm
-    rpos = np.array([int(x) for x in 5000 * lastRet / prcSoFar[:, -1]])
-    currentPos = np.array([int(x) for x in currentPos+rpos])
-    return currentPos
+def train_model(prices):
+    features = get_features(prices)
+    labels = get_labels(prices)
+    features = features[:, :-1, :]
+    X = features.reshape(-1, features.shape[-1])
+    y = labels.reshape(-1)
+    model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=42)
+    model.fit(X, y)
+    return model
+
+def getMyPosition(prices):
+    global trained_model, last_training_day
+    nInst, nDays = prices.shape
+    max_dollar_position = 10000
+    if nDays > 220 and (trained_model is None or nDays - last_training_day >= 10):
+        trained_model = train_model(prices[:, :-1])
+        last_training_day = nDays
+    if trained_model is None or nDays <= 220:
+        return np.zeros(nInst, dtype=int)
+    features_today = get_features(prices[:, -221:])[:, -1, :]
+    probs = trained_model.predict_proba(features_today)[:, 1]
+    confidence = np.abs(probs - 0.5)
+    direction = np.sign(probs - 0.5)
+    position_value = direction * confidence * max_dollar_position
+    today_prices = prices[:, -1]
+    position_in_shares = (position_value / today_prices).astype(int)
+    pos_limits = (max_dollar_position / today_prices).astype(int)
+    position_in_shares = np.clip(position_in_shares, -pos_limits, pos_limits)
+    return position_in_shares
